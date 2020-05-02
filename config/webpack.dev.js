bd146b1b283676b480f0f47b36e640e8ca84c767
Denis Sidorenko <denis_sidorenko@epam.com>
2020-05-02 17:36:21 +0300

    feat: add style loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -12,5 +12,20 @@ module.exports = {
     mode: 'development',
     devServer: {
         contentBase: 'dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            }
+        ]
     }
 };
